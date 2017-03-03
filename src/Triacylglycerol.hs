18e5110522410613585ef709789583f03b2cb9f8
Michael Thomas <Michaelt293@gmail.com>
2017-03-03 19:19:33 +1100

    Record wildcards added

--- src/Triacylglycerol.hs
+++ src/Triacylglycerol.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE RecordWildCards #-}
 module Triacylglycerol where
 
 import FattyAcid
@@ -39,7 +40,7 @@ instance Show Triacylglycerol where
 
 instance ToMolecularFormula Triacylglycerol where
   toMolecularFormula tg =
-    mkMolecularFormula [(C, 3), (H, 6)] |+|
+    mkMolecularFormula [(C, 3), (H, 6)] <>
     foldMap toMolecularFormula (tg^..allFAs)
 
 instance ToElementalComposition Triacylglycerol where
@@ -53,11 +54,11 @@ tagFAs tg = nub $ tg^..allFAs
 -- implementation if there are performance issues.
 possibleTAGs :: Double -> MonoisotopicMass -> [FattyAcyl] -> [Triacylglycerol]
 possibleTAGs n prec fas = nub $ do
-  fa1 <- fas
-  fa2 <- fas
-  fa3 <- fas
-  guard (withinTolerance n prec (I.monoisotopicMass (Triacylglycerol fa1 fa2 fa3)))
-  return $ Triacylglycerol fa1 fa2 fa3
+  _fa1 <- fas
+  _fa2 <- fas
+  _fa3 <- fas
+  guard . withinTolerance n prec $ I.monoisotopicMass Triacylglycerol {..}
+  return Triacylglycerol {..}
 
 findPossibleTAGs :: AssignedFAs -> [Triacylglycerol]
 findPossibleTAGs fas =
